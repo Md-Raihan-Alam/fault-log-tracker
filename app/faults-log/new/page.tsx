27b53b15d8d98048aaca437f46dcf241bc644a6e
Md-Raihan-Alam <mdraihanalam40@gmail.com>
2024-08-03 22:24:53 +0600

    feat(ui,api): handle form and post api added

--- app/faults-log/new/page.tsx
+++ app/faults-log/new/page.tsx
@@ -1,13 +1,43 @@
-import { Button, TextArea, TextField } from "@radix-ui/themes";
+"use client";
+import { Button, TextField } from "@radix-ui/themes";
 import React from "react";
+import SimpleMDE from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
+import { useForm, Controller } from "react-hook-form";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+
+interface FaultForm {
+  title: string;
+  description: string;
+}
 
 const NewFaultPage = () => {
+  const router = useRouter();
+  const { register, control, handleSubmit } = useForm<FaultForm>();
+
   return (
-    <div className="max-w-xl space-y-3">
-      <TextField.Root placeholder="Title"></TextField.Root>
-      <TextArea placeholder="Description"></TextArea>
+    <form
+      className="max-w-xl space-y-3"
+      onSubmit={handleSubmit(async (data) => {
+        await axios.post("/api/faults", data);
+        router.push("/faults-log");
+      })}
+    >
+      <TextField.Root
+        placeholder="Title"
+        {...register("title")}
+      ></TextField.Root>
+      <Controller
+        name="description"
+        control={control}
+        render={({ field }) => (
+          <SimpleMDE placeholder="Description" {...field} />
+        )}
+      />
+
       <Button>Submit New Fault</Button>
-    </div>
+    </form>
   );
 };
 
