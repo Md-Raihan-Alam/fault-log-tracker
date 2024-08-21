67d9623cc751aff9fc89a1778986927deec6410b
Md-Raihan-Alam <mdraihanalam40@gmail.com>
2024-08-21 22:57:41 +0600

    feat(ui): Handle errors for delete api

--- app/faults-log/[id]/DeleteFaultButton.tsx
+++ app/faults-log/[id]/DeleteFaultButton.tsx
@@ -2,41 +2,63 @@
 import { AlertDialog, Button, Flex } from "@radix-ui/themes";
 import axios from "axios";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 const DeleteFaultButton = ({ faultId }: { faultId: number }) => {
   const router = useRouter();
+  const [error, setError] = useState(false);
+  const deleteFaultLog = async () => {
+    try {
+      await axios.delete("/api/faults/" + faultId);
+      router.push("/faults-log");
+      router.refresh();
+    } catch (error) {
+      setError(true);
+    }
+  };
   return (
-    <AlertDialog.Root>
-      <AlertDialog.Trigger>
-        <Button color="red">Delete Fault-log</Button>
-      </AlertDialog.Trigger>
-      <AlertDialog.Content>
-        <AlertDialog.Title>Confirm Deletion</AlertDialog.Title>
-        <AlertDialog.Description>
-          Are you sure you want to delte this fault-log? This action can not be
-          undone
-        </AlertDialog.Description>
-        <Flex mt="4" gap="4">
-          <AlertDialog.Cancel>
-            <Button variant="soft" color="gray">
-              Cancel
-            </Button>
-          </AlertDialog.Cancel>
-          <AlertDialog.Action>
-            <Button
-              color="red"
-              onClick={async () => {
-                await axios.delete("/api/faults/" + faultId);
-                router.push("/faults-log");
-                router.refresh();
-              }}
-            >
-              Delete Fault-log
-            </Button>
-          </AlertDialog.Action>
-        </Flex>
-      </AlertDialog.Content>
-    </AlertDialog.Root>
+    <>
+      <AlertDialog.Root>
+        <AlertDialog.Trigger>
+          <Button color="red">Delete Fault-log</Button>
+        </AlertDialog.Trigger>
+        <AlertDialog.Content>
+          <AlertDialog.Title>Confirm Deletion</AlertDialog.Title>
+          <AlertDialog.Description>
+            Are you sure you want to delte this fault-log? This action can not
+            be undone
+          </AlertDialog.Description>
+          <Flex mt="4" gap="4">
+            <AlertDialog.Cancel>
+              <Button variant="soft" color="gray">
+                Cancel
+              </Button>
+            </AlertDialog.Cancel>
+            <AlertDialog.Action>
+              <Button color="red" onClick={deleteFaultLog}>
+                Delete Fault-log
+              </Button>
+            </AlertDialog.Action>
+          </Flex>
+        </AlertDialog.Content>
+      </AlertDialog.Root>
+      <AlertDialog.Root open={error}>
+        <AlertDialog.Content>
+          <AlertDialog.Title>Error</AlertDialog.Title>
+          <AlertDialog.Description>
+            This fault-log could not be deleted.
+          </AlertDialog.Description>
+          <Button
+            color="gray"
+            variant="soft"
+            mt="2"
+            onClick={() => setError(false)}
+          >
+            OK
+          </Button>
+        </AlertDialog.Content>
+      </AlertDialog.Root>
+    </>
   );
 };
 
